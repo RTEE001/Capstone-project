f958ecf3df65ba7e45803f74c139c7b7c3766c0a
Thifhidzi Rammbuda <rmmthi001@cs.uct.ac.za>
2022-09-16 13:40:22 +0200

    Changed databases

--- CBIB/settings.py
+++ CBIB/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'CBIB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CBIB',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
